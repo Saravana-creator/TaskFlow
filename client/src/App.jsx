bfcf414cefe6063365dbcd8cb80e9d62b2c9a825
Saravana-creator <saravana24057@gmail.com>
2026-04-06 10:44:22 +0530

    First commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-5xl font-bold text-blue-600">
+        Hello World
+      </h1>
+    </div>
+  )
+}
+
+export default App
